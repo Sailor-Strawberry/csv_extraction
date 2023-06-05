e3c6efbf92f813324de5c2d665c66b34470e5a36
Sailor-Strawberry <72081499+Sailor-Strawberry@users.noreply.github.com>
2023-06-05 11:44:44 +0900

    Update extraction.bat

--- extraction.bat
+++ extraction.bat
@@ -1,36 +1,82 @@
 @echo off
 
-REM 抽出対象のCSVファイル
-set csv_file=example.csv
+setlocal enabledelayedexpansion
 
-REM 抽出開始行の指定（0から始まるインデックス）
-set start_line=2
+REM 入力ファイルのパスと名前を指定してください
 
-REM 抽出する行の間隔
-set interval=3
+set /p input_file="分割したいファイル名を入力して下さい（.csvまで）:"
 
-REM ヘッダー行を抽出するかどうか
-set include_header=true
+REM 出力ファイルのパスと名前を指定してください
+for %%F in ("%input_file%") do set "filename=%%~nF"
+set "output_file=%filename%_split.csv"
 
-REM CSVファイルを処理する関数
-:process_csv
-set file="%csv_file%"
-for /f "usebackq tokens=*" %%a in (`more +%start_line% %file% ^| findstr /r /n "^"`) do (
-    set "line=%%a"
-    setlocal enabledelayedexpansion
-    set "line=!line:*:=!"
-    if "%include_header%"=="true" (
-        echo !line!
-    ) else (
-        set /a "line_number=%%a-%start_line%"
-        set /a "remainder=!line_number! %% %interval%"
-        if "!remainder!"=="0" (
-            echo !line!
+REM 抽出開始行と抽出間隔を指定してください（1から始まる行番号）
+
+set /p start_row="抽出開始indexを指定して下さい:"
+
+set /p interval="抽出間隔を指定して下さい:"
+
+REM ヘッダー行を抽出するため、ヘッダーの行数を指定してください
+
+set /p header_rows="何行目までをヘッダーとするか入力して下さい:" 
+
+REM ヘッダー行数を加味した抽出開始行を計算します
+
+set /a "adjusted_start_row=start_row + header_rows"
+
+REM 抽出処理を開始します
+
+echo Extracting rows from %input_file%...
+
+echo.
+
+REM ヘッダー行の抽出処理を開始します
+
+set /a "counter=0"
+
+for /f "usebackq delims=" %%a in ("%input_file%") do (
+
+    set /a "counter+=1"
+
+    if !counter! leq %header_rows% (
+
+        echo %%a
+
+    )
+
+    if !counter! equ %header_rows% (
+
+        goto :DataExtraction
+
+    )
+
+) >> "%output_file%"
+
+:DataExtraction
+
+REM データ行の抽出処理を開始します
+
+set /a "counter=0"
+set "skip_count=%adjusted_start_row%"
+for /f "usebackq tokens=*" %%a in ("%input_file%") do (
+    set /a "counter+=1"
+    if !counter! geq !skip_count! (
+        set /a "remainder=(counter - !adjusted_start_row!) %% %interval%"
+        if !remainder! equ 0 (
+            echo %%a
         )
     )
-    endlocal
-)
-goto :eof
 
-REM CSVファイルを処理する
-call :process_csv
+) >> "%output_file%"
+
+REM 抽出処理が完了しました
+
+echo.
+
+echo Extraction completed. Extracted rows are saved in %output_file%.
+
+echo.
+
+echo Press any key to exit.
+
+pause > nul
